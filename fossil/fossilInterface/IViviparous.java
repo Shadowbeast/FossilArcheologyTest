381aa8db82e8b6ed33f9dcf360350ea82cb66830
Shadow Beast <minecrawler@gmx.de>
2013-04-28 12:52:13 +0200

    The Version i cant get to load in MC 1.5.1

---
+++ fossil/fossilInterface/IViviparous.java
@@ -0,0 +1,11 @@
+package fossil.fossilInterface;
+
+import fossil.fossilEnums.EnumAnimalType;
+import fossil.guiBlocks.GuiPedia;
+
+public interface IViviparous
+{
+    void ShowPedia(GuiPedia var1);
+
+    void SetEmbryo(EnumAnimalType var1);
+}
